69bb3a162473593830b432a2006a688719b14a8d
Jesse Quijano <jesse@quijano.net>
2018-04-15 16:47:51 -0700

    Change name to support history.

--- templates/basepet.php
+++ templates/basepet.php
@@ -13,17 +13,24 @@ if(!interface_exists('iPet'))
 
 abstract class Pet implements iPet
 {
-    private $age;
+    protected $age;
     private $favoriteFood;
-    protected $name;
+    protected $name = array();
     protected $IsDeleted = 0;
     protected $petTypeId = 0;
+    protected $speaks = 1;
     public static $PETS = [0 => 'Unknown', 1 => 'Dog', 2 => 'Cat', 3 => 'Turtle', 4 => 'Bird', 5 => 'Unicorn'];
 
     public function __construct($Family, $Genus, $Species, $Subspecies, $petAge=0, $petName='', $petFood='')
     {
         $this->age = $petAge;
-        $this->name = $petName;
+
+        if($petName != '')
+        {
+            $this->setName($petName);
+        }
+
+
         $this->favoriteFood = $petFood;
     }
 
@@ -39,7 +46,7 @@ abstract class Pet implements iPet
 
     public function setName(string $petName) : void
     {
-        $this->name = $petName;
+        array_push($this->name, trim($petName));
     }
 
     public function setpetTypeId(int $petTypeId)
@@ -59,6 +66,11 @@ abstract class Pet implements iPet
 
     public function getName() : string
     {
+        return sizeof($this->name) > 0 ? $this->name[count($this->name)-1] : '';  //count/sizeof the same, just showing I know...
+    }
+
+    public function getNames() : array
+    {
         return $this->name;
     }
 
@@ -72,6 +84,20 @@ abstract class Pet implements iPet
         return $this->petTypeId;
     }
 
+    //TODO: add guard logic to ensure
+    public function getAverageNameLength() : int
+    {
+        /* find some fancier/lightning fast way to do this? */
+        $total = 0;
+        foreach($this->name as $name)
+        {
+            $total += strlen($name);
+        }
+
+        return (int)round($total / count($this->name), 0);
+    }
+
     abstract public static function toModel($obj);
     abstract public function getTrinomialName() : string;
+    abstract public function speak() : string;
 }
